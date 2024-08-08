0c9db82ecc5922a01148b9006a8537c068b6acba
siva <sivaprasad.s88@gmail.com>
2024-08-08 10:38:27 +0200

    [TASK] Implement new task

---
+++ brand-name.py
@@ -0,0 +1,7 @@
+"""
+A Band Name Generator
+"""
+print("Welcome to the Brand Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print(f"Your band name could be {city} {pet}")

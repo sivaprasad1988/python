389141edd8f1cb1ec27f16470ebf9028dfc8ea2f
Siva <s.sisupalan@gingco.de>
2024-08-08 10:15:56 +0200

    [TASK] Implement number of steps to zero

--- 1342-number-of-steps-to-zero.py
+++ 1342-number-of-steps-to-zero.py
@@ -7,11 +7,12 @@ class Solution(object):
         count = int(0)
         while num != 0:
             count += 1
-            if num %2 == 0:
+            if num % 2 == 0:
                 num = num / 2
             elif num % 2 == 1:
                 num = num - 1
         return count
 
+
 object = Solution()
-print( object.numberOfSteps(123) )
+print(object.numberOfSteps(123))
